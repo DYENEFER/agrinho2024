cf2d47a931d04ca37cf1309facdc98eee577bb2e
DYENEFER <127304417+DYENEFER@users.noreply.github.com>
2024-05-14 08:28:52 -0300

    u5uhrr

---
+++ main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(mostrarPopup, 2000);
+});
+
+function mostrarPopup() {
+    document.getElementById('popup').style.display = 'block';
+}
+
+function fecharPopup() {
+    document.getElementById('popup').style.display = 'none';
+}
